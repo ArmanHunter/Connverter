6025038499d495e7917174c073ac9264da29bedb
ArmanHunter <arman78hanter@gmail.com>
2022-12-13 02:27:55 +0700

    Изменены имена переменных.  Введена проверка на диапозон чисел.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,39 +12,47 @@ namespace Connverter
         static void Main(string[] args)
         {
             //int num;
-            Console.WriteLine("Введите одну из четырех цифр для выбора систем счисления :\r\n 1 - двоичная" + //выбор пользователя систем счисления из еоторой он хочет конвертировать число
+            Console.WriteLine("Введите одну из четырех цифр для выбора систем счисления (При вводе не цифр, программа не сработает!):\r\n 1 - двоичная" + //выбор пользователя систем счисления из еоторой он хочет конвертировать число
 
                 " \r\n 2 - восьмиричная \r\n 3 - десятичная \r\n 4 - шестнадцатиричная ");
 
-            //Console.WriteLine("выберите число");
 
-            int num = Convert.ToInt32(Console.ReadLine());
+            int numberSystem = Convert.ToInt32(Console.ReadLine());
 
-            {
-                if (num == 1) // выбор числа 1-значит двоичная сс.
+            { 
+                if (numberSystem <= 0 || numberSystem >= 5)
+                {
+                    Console.WriteLine("Ошибка! Диапазон от 1 до 4!");
+                }
+            
+                if (numberSystem == 1) // выбор числа 1-значит двоичная сс.
                 {
                     Console.WriteLine("Вы выбрали 2-ую систему. Теперь выберите систему в которую хотите конвертировать число из 2-чной системы");
-                    int number1 = Convert.ToInt32(Console.ReadLine()); 
-                    if (number1 == 1) // выбор второй сс в которую пол-ль хочет конвертировать число
+                    int binaryNumber = Convert.ToInt32(Console.ReadLine());
+                    if (binaryNumber <= 0 || binaryNumber >= 5)
+                    {
+                        Console.WriteLine("Ошибка! Диапазон от 1 до 4!");
+                    }
+                    if (binaryNumber == 1) // выбор второй сс в которую пол-ль хочет конвертировать число
                     {
                         Console.WriteLine("Вы выбрали 2-ую систему. Переводить из 2-ной системы в 2-ную нет смысла");
 
                     }
-                    if (number1 == 2) // выбор второй сс в которую пол-ль хочет конвертировать число
+                    if (binaryNumber == 2) // выбор второй сс в которую пол-ль хочет конвертировать число
                     {
                         Console.WriteLine("Вы выбрали 8-ую систему. Теперь наберите число в 2-ной системе для конвертации");
-                        int numb2 = Convert.ToInt32(Console.ReadLine(),2);// запонимаем введенное число
+                        int numb2 = Convert.ToInt32(Console.ReadLine(), 2);// запонимаем введенное число
                         string s = Convert.ToString(numb2, 8);// конвертируем в 8-ную сс
                         Console.WriteLine("В восьмеричной системе счисления это: " + s);// выводим готовый результат
                     }
-                    if (number1 == 3)
+                    if (binaryNumber == 3)
                     {
                         Console.WriteLine("Вы выбрали 10-ую систему. Теперь наберите число в 2-ной системе для конвертации");
                         int numb2 = Convert.ToInt32(Console.ReadLine(), 2);
                         string s = Convert.ToString(numb2, 10);
                         Console.WriteLine("В десятичной системе счисления это: " + s);
                     }
-                    if (number1 == 4)
+                    if (binaryNumber == 4)
                     {
                         Console.WriteLine("Вы выбрали 16-ую систему. Теперь наберите число в 2-ной системе для конвертации: \r\n");
                         int numb2 = Convert.ToInt32(Console.ReadLine(), 2);
@@ -51,11 +60,15 @@ namespace Connverter
                         Console.WriteLine("В 16-ной системе счисления это: " + s);
                     }
                 }
-                if (num == 2)
+                if (numberSystem == 2)
                 {
                     Console.WriteLine("Вы выбрали 8-ную систему. Теперь выберите систему в которую хотите конвертировать число из 8-чной системы");
-                    int number2 = Convert.ToInt32(Console.ReadLine());
-                    if (number2 == 1)
+                    int octalNumber = Convert.ToInt32(Console.ReadLine());
+                    if (octalNumber <= 0 || octalNumber >= 5)
+                    {
+                        Console.WriteLine("Ошибка! Диапазон от 1 до 4!");
+                    }
+                    if (octalNumber == 1)
                     {
                         Console.WriteLine("Вы выбрали 2-ую систему. Теперь наберите в 8-ной системе число для конвертации");
                         int numb2 = Convert.ToInt32(Console.ReadLine(), 8);
@@ -63,81 +76,89 @@ namespace Connverter
                         Console.WriteLine("В 2-ной системе счисления это: " + s);
 
                     }
-                    if (number2 == 2)
+                    if (octalNumber == 2)
                     {
                         Console.WriteLine(" Вы выбрали 8-ую систему. Переводить из 8-ной системы в 8-ную нет смысла");
                         int numb2 = Convert.ToInt32(Console.ReadLine());
                         Convert.ToString(numb2, 8);
 
                     }
-                    if (number2 == 3)
+                    if (octalNumber == 3)
                     {
                         Console.WriteLine("Вы выбрали 10-ую систему. Теперь наберите в 8-ной системе число для конвертации");
                         int numb2 = Convert.ToInt32(Console.ReadLine(), 8);
                         string s = Convert.ToString(numb2, 10);
-                        Console.WriteLine("В 2-ной системе счисления это: " + s);
+                        Console.WriteLine("В 10-ной системе счисления это: " + s);
                     }
-                    if (number2 == 4)
+                    if (octalNumber == 4)
                     {
                         Console.WriteLine("Вы выбрали 16-ую систему. Теперь наберите в 8-ной системе число для конвертации");
                         int numb2 = Convert.ToInt32(Console.ReadLine(), 8);
                         string s = Convert.ToString(numb2, 16);
-                        Console.WriteLine("В 2-ной системе счисления это: " + s);
+                        Console.WriteLine("В 16-ной системе счисления это: " + s);
                     }
                 }
-                    if (num == 3)
-                    {
+                if (numberSystem == 3)
+                {
                     Console.WriteLine("Вы выбрали 10-ую систему. Теперь выберите систему в которую хотите конвертировать число из 10-чной системы");
-                    int number3 = Convert.ToInt32(Console.ReadLine());
-                    if (number3 == 1)
-                        {
+                    int decimalNumber = Convert.ToInt32(Console.ReadLine());
+                    if (decimalNumber <= 0 || decimalNumber >= 5)
+                    {
+                        Console.WriteLine("Ошибка! Диапазон от 1 до 4!");
+                    }
+                    if (decimalNumber == 1)
+                    {
                         Console.WriteLine("Вы выбрали 2-ую систему.Теперь наберите число в 10-ной системе для конвертации: ");
                         int numb2 = Convert.ToInt32(Console.ReadLine(), 10);
                         string s = Convert.ToString(numb2, 2);
                         Console.WriteLine("В 2-ной системе счисления это: " + s);
                     }
-                    if (number3 == 2)
-                        {
+                    if (decimalNumber == 2)
+                    {
                         Console.WriteLine("Вы выбрали 8-ую систему.Теперь наберите число в 10-ной системе для конвертации: ");
                         int numb2 = Convert.ToInt32(Console.ReadLine(), 10);
                         string s = Convert.ToString(numb2, 8);
                         Console.WriteLine("В 8-ной системе счисления это: " + s);
                     }
-                        if (number3 == 3)
-                        {
+                    if (decimalNumber == 3)
+                    {
 
-                            Console.WriteLine("Вы выбрали 10 - ую систему. Переводить из 10-ной системы в 10-ную нет смысла");
+                        Console.WriteLine("Вы выбрали 10 - ую систему. Переводить из 10-ной системы в 10-ную нет смысла");
 
-                        }
-                        if (number3 == 4)
-                        {
-                            Console.WriteLine("Вы выбрали 16-ую систему. Теперь наберите в 10-ной системе число для конвертации");
+                    }
+                    if (decimalNumber == 4)
+                    {
+                        Console.WriteLine("Вы выбрали 16-ую систему. Теперь наберите в 10-ной системе число для конвертации");
                         int numb2 = Convert.ToInt32(Console.ReadLine(), 10);
                         string s = Convert.ToString(numb2, 16);
                         Console.WriteLine("В 16-ной системе счисления это: " + s);
                     }
-                     }
-                    if (num == 4)
+                }
+                if (numberSystem == 4)
+                {
+                    Console.WriteLine("Вы выбрали 16-ную систему. Теперь выберите систему в которую хотите конвертировать число из 16-чной системы");
+                    int hexadecimalNumber = Convert.ToInt32(Console.ReadLine());
+                    if (hexadecimalNumber <= 0 || hexadecimalNumber >= 5)
                     {
-                        Console.WriteLine("Вы выбрали 16-ную систему. Теперь выберите систему в которую хотите конвертировать число из 16-чной системы");
-                        int number4 = Convert.ToInt32(Console.ReadLine());
-                        if (number4 == 1)
-                        {
+                        Console.WriteLine("Ошибка! Диапазон от 1 до 4!");
+                    }
+                    if (hexadecimalNumber == 1)
+                    {
                         Console.WriteLine("Вы выбрали 2-ую систему.Теперь наберите число в 16-ной системе для конвертации: ");
                         int numb2 = Convert.ToInt32(Console.ReadLine(), 16);
                         string s = Convert.ToString(numb2, 2);
                         Console.WriteLine("В 2-ной системе счисления это: " + s);
 
                     }
-                        if (number4 == 2)
-                        {
+                    if (hexadecimalNumber == 2)
+                    {
                         Console.WriteLine("Вы выбрали 8-ую систему.Теперь наберите число в 16-ной системе для конвертации: ");
                         int numb2 = Convert.ToInt32(Console.ReadLine(), 16);
                         string s = Convert.ToString(numb2, 8);
                         Console.WriteLine("В 8-ной системе счисления это: " + s);
                     }
-                        if (number4 == 3)
-                        {
+                    if (hexadecimalNumber == 3)
+                    {
 
                         Console.WriteLine("Вы выбрали 10-ую систему.Теперь наберите число в 16-ной системе для конвертации: ");
                         int numb2 = Convert.ToInt32(Console.ReadLine(), 16);
@@ -145,12 +166,12 @@ namespace Connverter
                         Console.WriteLine("В 10-ной системе счисления это: " + s);
 
                     }
-                        if (number4 == 4)
-                        {
-                            Console.WriteLine("Вы выбрали 16-ую систему. Переводить из 16-ной системы в 16-ную нет смысла");
+                    if (hexadecimalNumber == 4)
+                    {
+                        Console.WriteLine("Вы выбрали 16-ую систему. Переводить из 16-ной системы в 16-ную нет смысла");
 
-                        }
                     }
+                }
                 Console.ReadKey();
             }
 
